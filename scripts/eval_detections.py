b856648d35eddbb8590f1953973c97ef5ae65786
z430 <maystya@gmail.com>
2025-05-11 07:16:28 +0700

    Add COCO labels and update evaluation logic in eval_detections script

--- scripts/eval_detections.py
+++ scripts/eval_detections.py
@@ -3,13 +3,98 @@ from loguru import logger
 from datetime import datetime
 
 import fiftyone as fo
+import cv2
 import torch
 from clearml import Task, OutputModel
 import shutil
 import os
+import tqdm
+import os
+from pathlib import Path
 
 PROJECT_NAME = "DetectionEvaluation"
-
+COCO_LABELS = {
+    "person": 0,
+    "bicycle": 1,
+    "car": 2,
+    "motorcycle": 3,
+    "airplane": 4,
+    "bus": 5,
+    "train": 6,
+    "truck": 7,
+    "boat": 8,
+    "traffic light": 9,
+    "fire hydrant": 10,
+    "stop sign": 11,
+    "parking meter": 12,
+    "bench": 13,
+    "bird": 14,
+    "cat": 15,
+    "dog": 16,
+    "horse": 17,
+    "sheep": 18,
+    "cow": 19,
+    "elephant": 20,
+    "bear": 21,
+    "zebra": 22,
+    "giraffe": 23,
+    "backpack": 24,
+    "umbrella": 25,
+    "handbag": 26,
+    "tie": 27,
+    "suitcase": 28,
+    "frisbee": 29,
+    "skis": 30,
+    "snowboard": 31,
+    "sports ball": 32,
+    "kite": 33,
+    "baseball bat": 34,
+    "baseball glove": 35,
+    "skateboard": 36,
+    "surfboard": 37,
+    "tennis racket": 38,
+    "bottle": 39,
+    "wine glass": 40,
+    "cup": 41,
+    "fork": 42,
+    "knife": 43,
+    "spoon": 44,
+    "bowl": 45,
+    "banana": 46,
+    "apple": 47,
+    "sandwich": 48,
+    "orange": 49,
+    "broccoli": 50,
+    "carrot": 51,
+    "hot dog": 52,
+    "pizza": 53,
+    "donut": 54,
+    "cake": 55,
+    "chair": 56,
+    "couch": 57,
+    "potted plant": 58,
+    "bed": 59,
+    "dining table": 60,
+    "toilet": 61,
+    "tv": 62,
+    "laptop": 63,
+    "mouse": 64,
+    "remote": 65,
+    "keyboard": 66,
+    "cell phone": 67,
+    "microwave": 68,
+    "oven": 69,
+    "toaster": 70,
+    "sink": 71,
+    "refrigerator": 72,
+    "book": 73,
+    "clock": 74,
+    "vase": 75,
+    "scissors": 76,
+    "teddy bear": 77,
+    "hair drier": 78,
+    "toothbrush": 79
+}
 
 def load_model(model_path: str):
     model = torch.hub.load(
@@ -20,6 +105,40 @@ def load_model(model_path: str):
     )
     return model
 
+def eval(dataset, model):
+    for sample in tqdm.tqdm(dataset, desc="Evaluating samples", total=len(dataset)):
+        sample_id = sample.id
+        try:
+            results = model(sample.filepath)
+            img = cv2.imread(sample.filepath)
+            height, width, _ = img.shape
+            predictions = results.pandas().xyxy[0]
+            detections = []
+            for _, row in predictions.iterrows():
+                label = row["name"]
+                confidence = row["confidence"]
+                x_min = row["xmin"]
+                y_min = row["ymin"]
+                x_max = row["xmax"]
+                y_max = row["ymax"]
+                detections.append(
+                    fo.Detection(
+                        label=label,
+                        confidence=confidence,
+                        bounding_box=[
+                            x_min / width,
+                            y_min / height,
+                            (x_max - x_min) / width,
+                            (y_max - y_min) / height,
+                        ],
+                    )
+                )
+            sample["preds_v20250510"] = fo.Detections(detections=detections)
+            sample.save()
+        except Exception as e:
+            logger.error(f"Failed to evaluate sample: {sample_id}. Error: {e}")
+
+    return
 
 def main(args):
     try:
@@ -32,44 +151,26 @@ def main(args):
     task_name = clearml_task.name
     model_output = clearml_task.get_models()["output"][-1]
     model_path = model_output.get_weights()
+    training_time = clearml_task._get_last_update()
 
     # evaluate
-    model = load_model(model_path)
-    for sample in dataset:
-        sample_id = sample.id
-        logger.info(f"Evaluating sample: {sample_id}")
-        try:
-            results = model(sample.filepath)
-            predictions = results.pandas().xyxy[0]
-            for _, row in predictions.iterrows():
-                label = row["name"]
-                confidence = row["confidence"]
-                x_min = row["xmin"]
-                y_min = row["ymin"]
-                x_max = row["xmax"]
-                y_max = row["ymax"]
-                print(
-                    f"Detected {label} with confidence {confidence} at "
-                    f"({x_min}, {y_min}) to ({x_max}, {y_max})"
-                )
-
-            logger.info(f"Detections saved for sample: {sample_id}")
-        except Exception as e:
-            logger.error(f"Failed to evaluate sample: {sample_id}. Error: {e}")
-
-    return
+    # model = load_model(model_path)
+    new_model_name = (
+        f"{args.model_name.lower()}_v{training_time.strftime('%Y%m%d')}.pt"
+    )
+    
     task_name = (
         f"{args.model_name.upper()} - {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
     )
     task = Task.init(project_name=PROJECT_NAME, task_name=task_name)
-    new_model_name = (
-        f"{args.model_name.lower()}_v{datetime.now().strftime('%Y%m%d%H%M%S')}.pt"
-    )
+
+    # eval 
+    # eval(dataset, model_path)
 
     # Create a new output model
     output_model = OutputModel(
         task=task,
-        label_enumeration={"person": 1, "car": 2, "bus": 3, "bike": 4},
+        label_enumeration=COCO_LABELS,
         name=new_model_name,
         framework="pytorch",
     )
@@ -80,12 +181,11 @@ def main(args):
             new_model_name,
         ),
     )
-    output_model.update_weights_package(
-        os.path.join(os.path.dirname(model_path), new_model_name)
-    )
-
-    # evaluate
-
+    logger.info(f"{model_path} copied to {os.path.join(os.path.dirname(model_path), new_model_name)}")
+    logger.info(os.path.join(os.path.dirname(model_path), new_model_name))
+    
+    new_model_path = Path(os.path.join(os.path.dirname(model_path), new_model_name)) 
+    output_model.update_weights(weights_filename=str(Path(new_model_path).resolve()))
 
 def parse_arguments():
     parser = argparse.ArgumentParser(
